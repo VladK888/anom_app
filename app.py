4e74e89bb4564c953ee781d0102a8c523c4c655d
VladK888 <vkaleev2@gmail.com>
2024-09-25 22:12:59 +0200

    add data

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from scripts.preprocessing import preprocess_data
-
+from scripts.preprocessing import visualization
 
 
 
@@ -12,9 +12,12 @@ data = pd.read_csv(file_path)
 # Preprocess data
 data = preprocess_data(data)
 
-st.title("Time corridors")
+st.title("The Temporal Corridors")
 
-st.write("Disclaimer The Temporal Corridors application provides only statistical information and data for the analysis of market assets. The company assumes no responsibility for any financial losses, damages, or other consequences resulting from the use of this application. The data presented and historical performance are not guarantees of future results and should not be construed as investment advice. Users make their own decisions related to trading or investing and bear full responsibility for their actions. Before making any financial transactions, it is strongly recommended to consult a professional financial advisor.")
+plot_price_chart(data)
+
+st.write("Disclaimer ")
+st.write("The Temporal Corridors application provides only statistical information and data for the analysis of market assets. The company assumes no responsibility for any financial losses, damages, or other consequences resulting from the use of this application. The data presented and historical performance are not guarantees of future results and should not be construed as investment advice. Users make their own decisions related to trading or investing and bear full responsibility for their actions. Before making any financial transactions, it is strongly recommended to consult a professional financial advisor.")
 
 
 
