8741df3867d64cc2953d7767a93ad408eaf9fbfc
VladK888 <vkaleev2@gmail.com>
2024-09-26 00:31:48 +0200

    add data

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from scripts.visualization import plot_price_chart
+from scripts.analyzer import analyzer
 
 st.title("The Temporal Corridors")
 
-plot_price_chart()
+analyzer()
 
 
 
